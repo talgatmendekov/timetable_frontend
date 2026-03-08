54674f91f58ded752f0241a71eab671c2301f5a2
talgatmendekove <talgatmendekov@gmail.com>
2026-03-08 22:35:28 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import "./mobile-fixes.css"
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
